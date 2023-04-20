35604c6eb74828efdb6886c73eb0627d97e84ac2
Edson <edbdelima@gmail.com>
2023-04-20 20:48:36 -0300

    Adicionar arquivos de projeto.

---
+++ Send.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Olá Mundo!";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty, 
+                     routingKey: "hello", 
+                     basicProperties: null, 
+                     body: body);
+
+Console.WriteLine($" [x] Enviado {message}");
+
+Console.WriteLine($"Pressione [enter] para sair");
+Console.ReadLine();
